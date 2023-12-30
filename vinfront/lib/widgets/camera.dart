b5c379039007b9ac876ae43845e66af2dce5ee0f
Sebastien <sebastien.re@alumni.univ-avignon.fr>
2023-12-30 15:56:32 +0100

    prise de photo + retour

--- vinfront/lib/widgets/camera.dart
+++ vinfront/lib/widgets/camera.dart
@@ -1,4 +1,6 @@
 // camera_page.dart
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
@@ -32,14 +34,62 @@ class _CameraPageState extends State<CameraPage> {
     }
   }
 
+  void takePhoto() async {
+    if (!controller.value.isInitialized) {
+      return;
+    }
+    final image = await controller.takePicture();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ImagePreviewPage(imagePath: image.path),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     if (controller == null || !controller.value.isInitialized) {
       return Container();
     }
-    return AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: CameraPreview(controller),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera Page'),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            constraints: BoxConstraints.expand(),
+            child: CameraPreview(controller),
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            child: FloatingActionButton(
+              onPressed: takePhoto,
+              child: Icon(Icons.camera),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagePreviewPage extends StatelessWidget {
+  final String imagePath;
+
+  const ImagePreviewPage({required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Preview'),
+      ),
+      body: Center(
+        child: Image.file(File(imagePath)),
+      ),
     );
   }
 }
